b3e3e4c75dc5f0eb4696f7ff2dac53943f6d9898
Andrey Ovcharov <>
2022-11-12 23:14:42 +0100

    Test application

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container mx-auto">
+    <div v-masonry>
+      <div v-masonry-tile v-for="item, index in items" :key="index" class="w-1/3">
+        {{ item }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data: () => ({
+    items: [
+      1, 2, 3, 4, 5, 6, 7, 8
+    ]
+  })
+}
+</script>
